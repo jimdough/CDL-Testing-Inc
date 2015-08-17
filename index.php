804986fd19a447d321c4669c27c244c5c41bf375
Jim D <jimdough@MacBook-Pro.local>
2015-08-17 16:04:54 -0400

    Initial Sync

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CDL Testing Inc.</title>
+  <?php include("inc/head.inc"); ?>
+  </head>
+  <body>
+    
+   <!-- Nav and Banner -->
+ 
+		   <?php include("inc/nav.inc"); ?>
+
+  <div class="row">
+    <div class="large-4 columns">
+      <p></p>
+      <h3>This is the intro text!</h3>
+      <p>Put word stuff here. Chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+   
+    <div class="large-8 columns">
+	  <p></p>
+      <p><img src="img/hero.jpg" /></p>
+    </div>
+    
+  </div>
+  
+  <!-- End Nav and Banner -->
+  
+  
+  <!-- Main Content Section -->
+  
+  <div class="row">
+  
+    <div class="large-12 columns">
+      <h4>This is a content section.</h4>
+      <p>More wordy things here.Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+      <p>More wordy things here.Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+    </div>
+    
+        
+  </div>
+  
+  
+  <!-- Call to Action Panel -->
+  <div class="row">
+    <div class="large-12 columns">
+    
+      <div class="panel">
+        <h4>Get in touch with us at one of our locations:</h4><p></p>
+        
+        <div class="row">
+	        <div class="small-12 columns">
+		        <ul class="small-block-grid-2">
+					<li><h5>Indianapolis, IN</h5>4351 S Harding St<br/>Indianapolis,IN 46217</li>
+					<li><h5>Delphi, IN</h5>1576 W. US RT 421<br />Delphi, IN 46923</li>			
+				</ul>
+	        </div>
+        </div>
+            
+        <div class="row">
+          <div class="large-9 columns">
+            <p>We'd love to hear from you.</p>
+          </div>
+          <div class="large-3 columns">
+            <a href="#" class="radius button right">Contact Us</a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+  
+  <!-- Footer -->
+  
+    <?php include("inc/footer.inc"); ?>
+  
+     <?php include("inc/scripts.inc"); ?>
+
+  </body>
+</html>
